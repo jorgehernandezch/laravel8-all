34cc88fa89ac8495216917c89ef484ed2af7ec53
Jorge Edo. Hernández <jorge@galpha.co>
2021-03-24 17:12:59 -0300

    Template AdminLTE 3

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
