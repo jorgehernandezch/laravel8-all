02746baa73dab61ea7b3c4b59fa947b6bb3e0cee
jorgehernandezch <jorge@galpha.co>
2022-10-20 16:52:51 -0300

    Install Sail - Setup Docker

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.index');
+Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
